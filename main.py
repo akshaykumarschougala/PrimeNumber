34b0fd533ca78ab79c9973f4fdb0e2f87312e2ea
akshayakumarcho <32309722-akshayakumarcho@users.noreply.replit.com>
2024-02-23 13:59:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+N = int(input("Enter the value of N: "))
+M = int(input("Enter the value of M: "))
+if N > M:
+  Print("N is greater than M")
+  # Code block for true condition
+else:
+  print("M is greater than N")
+  # Code block for false condition
+  # Taking input for variables N and M
